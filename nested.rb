a875c95eced30ed2ea763043fff303fc9c7c8bf7
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-16 01:33:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash.merge[:yukihiro_matsumoto] = {}
+programmer_hash.merge[yukihiro_matsumoto: {known_for:}
     programmer_hash[:yukihiro_matsumoto]
 end
 
